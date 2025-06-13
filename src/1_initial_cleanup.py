a50a185e502fa3a81082e7b0e57b1edbb1da4063
ocarmo <omscarmo@gmail.com>
2025-06-13 09:33:38 -0500

    moved function up, updated comments

--- src/1_initial_cleanup.py
+++ src/1_initial_cleanup.py
@@ -1,3 +1,6 @@
+"""Imports data as numpy array
+"""
+
 import os
 import numpy as np
 from loguru import logger
@@ -6,35 +9,13 @@ from aicsimageio.writers.ome_tiff_writer import OmeTiffWriter
 
 logger.info('Import ok')
 
-input_path = "P:/Sophie/Uptake/crotamine/04082025_2503C/2025-04-08"
+input_path = "raw_data"
 output_folder = 'results/initial_cleanup/'
 
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
 
-# ---------------Initialize file_list---------------
-# read in all folders with image files
-valid_extensions = ['.czi', '.tif', '.tiff', '.lif']
-file_list = [[f'{filename}' for filename in files if any(ext in filename.lower() for ext in valid_extensions)]
-             for root, dirs, files in os.walk(f'{input_path}')]
 
-# flatten file_list
-file_list = [item for sublist in file_list for item in sublist]
-
-# include anything that you don't want quantitated 
-do_not_quantitate = []
-
-# ---------------Collect image names & convert---------------
-image_names = []
-for filename in file_list:
-    if all(word not in filename for word in do_not_quantitate):
-        name = os.path.splitext(filename)[0]  # works for .czi, .tif, .lif
-        image_names.append(name)
-
-# remove duplicates
-image_names = list(dict.fromkeys(image_names))
-
-# ---------------Modified converter function---------------
 def image_converter(image_name, input_folder, output_folder, tiff=False, array=True):
     """Convert and save images from supported formats"""
     if not os.path.exists(output_folder):
@@ -58,6 +39,29 @@ def image_converter(image_name, input_folder, output_folder, tiff=False, array=T
 
     if array:
         np.save(f'{output_folder}{image_name}.npy', image)
+
+
+# ---------------Initialize file_list---------------
+# read in all folders with image files
+valid_extensions = ['.czi', '.tif', '.tiff', '.lif']
+file_list = [[f'{filename}' for filename in files if any(ext in filename.lower() for ext in valid_extensions)]
+             for root, dirs, files in os.walk(f'{input_path}')]
+
+# flatten file_list
+file_list = [item for sublist in file_list for item in sublist]
+
+# flag substrings of images you do not want processed, e.g., 'UT' for all untransfected controls
+do_not_quantitate = []
+
+# ---------------Collect image names & convert---------------
+image_names = []
+for filename in file_list:
+    if all(word not in filename for word in do_not_quantitate):
+        name = os.path.splitext(filename)[0]  # works for .czi, .tif, .lif
+        image_names.append(name)
+
+# remove duplicates
+image_names = list(dict.fromkeys(image_names))
 
 # ---------------Run conversion---------------
 for name in image_names:
