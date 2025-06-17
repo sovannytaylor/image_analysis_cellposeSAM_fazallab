791e62b039abac5cbc057d24b695f6639e4ab939
ocarmo <omscarmo@gmail.com>
2025-06-17 11:29:37 -0500

    removed unused imports, added example data wrangling, commented out stats

--- src/5_visualization.py
+++ src/5_visualization.py
@@ -1,33 +1,20 @@
 """Visualize feature information collected in prior script"""
 
 import os
-import numpy as np
 import seaborn as sns
 import pandas as pd
-import math
-import matplotlib
 import matplotlib.pyplot as plt
-import skimage.io
-import functools
-import cv2
-from skimage import measure, segmentation, morphology
-from scipy.stats import skewtest, skew
-from skimage import morphology
-from skimage.morphology import remove_small_objects
 from statannotations.Annotator import Annotator
 from loguru import logger
-from matplotlib_scalebar.scalebar import ScaleBar
 plt.rcParams.update({'font.size': 14})
 
 input_folder = 'results/summary_calculations/'
 plotting_folder = 'results/plotting/'
 
-
 if not os.path.exists(plotting_folder):
     os.mkdir(plotting_folder)
 
-### ---- Load in the excel sheet as a dataframe ---------
-
+# ---------------- load in excel sheet as dataframe ----------------
 input_files = [f for f in os.listdir(input_folder) if f.endswith(('.xlsx', '.csv'))]
 file_path = os.path.join(input_folder, input_files[0])
 
@@ -39,11 +26,21 @@ else:
     raise ValueError("Unsupported file format. Expected .xlsx or .csv.")
 logger.info('file initialization complete :)')
 
+# ---------------- wrangling ----------------
+# add columns for sorting
+# example of how to make a new column 
+# this is saying take the df summary and add a column 'peptide'
+# look in column 'image_name' and look at the first item as delimited by the '_' 
+# example: 'date_PR30_lpd_63x_file'
+# this would take PR30 because its the first item if you split by the '_'
+summary_df['peptide'] = summary_df['image_name'].str.split('_').str[1]
+# this would take the lpd
+summary_df['condition'] = summary_df['image_name'].str.split('_').str[2]
+# this is how you do it for a substring in between two delimiters 
+summary_df['rep'] = summary_df['image_name'].str.split('_').str[-1].str.split('-').str[0]
 
-### --- Visualize 
-
-## I want to look at conditions for each peptide, comparing each condition against other conditions for that single peptide 
-
+# ---------------- visualize ----------------
+# I want to compare conditions for each peptide
 # Loop through each unique peptide
 for peptide in summary_df['peptide'].unique():
     
@@ -51,27 +48,26 @@ for peptide in summary_df['peptide'].unique():
     summary_df_peptide = summary_df[summary_df['peptide'] == peptide]
     summary_df_reps_peptide = summary_df[summary_df['peptide'] == peptide]
 
-    order = ['norm','norm_spike','LPD','LPD_spike','starv','starv_spike']
+    order = ['lpd']
     x = 'condition'
-   
 
     for parameter in ['mean_puncta_area', 'puncta_area_proportion', 'puncta_count',  'puncta_mean_minor_axis', 'puncta_mean_major_axis', 'avg_eccentricity', 'cell_intensity_mean']:
         # plot data
         fig, ax = plt.subplots()
-        ax = sns.stripplot(data=summary_df, x=x, y=parameter, order=order, dodge='True',
-                        edgecolor='white', linewidth=1, size=8, alpha=0.4)
+        ax = sns.stripplot(data=summary_df, x=x, y=parameter, order=order, dodge='True', edgecolor='white', linewidth=1, size=8, alpha=0.4)
         sns.stripplot(data=summary_df, x=x, y=parameter, order=order, dodge='True',
                         edgecolor='k', linewidth=1, size=8, ax=ax)
         sns.boxplot(data=summary_df, x=x, y=parameter,
                     order=order, palette=['.7', '.8'], ax=ax)
-        # statannot stats
-        pairs = [(condition, 'norm') for condition in order if condition != 'norm']
-        annotator = Annotator(ax, pairs, data=summary_df, x=x, y=parameter, order=order)
-        annotator.configure(test='Mann-Whitney', verbose=2)
-        annotator.apply_test()
-        annotator.annotate()
 
-        #add a title that also includes peptide iterated through 
+        # # statannotation stats
+        # pairs = [(condition, 'norm') for condition in order if condition != 'norm']
+        # annotator = Annotator(ax, pairs, data=summary_df, x=x, y=parameter, order=order)
+        # annotator.configure(test='Mann-Whitney', verbose=2)
+        # annotator.apply_test()
+        # annotator.annotate()
+
+        #a dd a title that also includes peptide iterated through 
         ax.set_title(f'{parameter} for {peptide}')
         # formatting
         sns.despine()
